3551731328e544dbb03353d48d371dcca6f687ce
Huihan <hh2720@nyu.edu>
2023-04-03 12:22:39 -0400

    Test

---
+++ Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+{
+
+    public float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 newPos = transform.position;
+
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            newPos.x -= speed;
+        }
+
+        Debug.Log(newPos.x);
+
+        transform.position = newPos;
+    }
+}
